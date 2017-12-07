cd012bba9e0d627327fdc2b55176d386c1444e3b
Ariful Islam Rajib <arif.rajib@gmail.com>
2017-12-06 22:27:54 -0800

    String object length

---
+++ reference/stringobject.js
@@ -0,0 +1,9 @@
+console.log('*********** String.length *********');
+/**
+String.length: This property returns the number of code units in the string
+*/
+var name = 'Md Ariful Islam Rajib';
+console.log(name.length);   // 21
+
+
+
