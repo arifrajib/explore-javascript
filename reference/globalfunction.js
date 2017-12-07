f8a203283c5ad4ab8ab62a7a5f71f82fd0779c19
Ariful Islam Rajib <arif.rajib@gmail.com>
2017-12-06 21:42:43 -0800

    Global function isNaN

--- reference/globalfunction.js
+++ reference/globalfunction.js
@@ -1,3 +1,4 @@
+console.log('*********** eval *********');
 /**
 eval: evaluates JavaScript code represented as a string.
 */
@@ -10,7 +11,8 @@ var str = "if (x == 5) {console.log('z is 42'); z = 42; x = 420;} else z = 0;";
 console.log('x is ', eval(str)); // z is 42 x is 420
 
 
-
+console.log('\n\n');
+console.log('*********** isFinite *********');
 /**
 isFinite: determines whether the passed value is a finite number.
 */
@@ -25,5 +27,25 @@ console.log(isFinite(-910));       // true
 
 console.log(isFinite(null));      // true,
 console.log(isFinite('0'));       // true
+
+
+
+console.log('\n\n');
+console.log('*********** isNaN *********');
+/**
+isNaN: function determines whether a value is NaN or not.
+*/
+console.log(isNaN(NaN));       // true
+console.log(isNaN(undefined)); // true
+console.log(isNaN({}));        // true
+
+console.log(isNaN(true));      // false
+console.log(isNaN(null));      // false
+console.log(isNaN(37));        // false
+
+// strings
+console.log(isNaN('37'));      // false
+
+
 
 
