0952377fce6316aee4169b56d9f0fb39b97b587b
Ariful Islam Rajib <arif.rajib@gmail.com>
2017-12-12 08:26:35 -0800

    Code output eval function

--- code_out_put/codeoutput.js
+++ code_out_put/codeoutput.js
@@ -8,6 +8,28 @@ var y = 1;
     y += typeof f;
   }
 console.log(y);
+/**
+Output will be "1undefined"
+*/
 
 
+var k = 1;
+  if (1) {
+    eval(function foo(){});
+    k += typeof foo;
+  }
+console.log(k);
+/**
+Output will be "1undefined"
+*/
 
+
+var k = 1;
+  if (1) {
+    function foo(){};
+    k += typeof foo;
+  }
+console.log(k);
+/**
+Output will be "1function"
+*/
